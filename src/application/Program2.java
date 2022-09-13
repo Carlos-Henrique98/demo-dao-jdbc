19e6f8bd0ea9e9a4293ff02968b55c60359abb5a
Carlos Henrique <ichfs9108@gmail.com>
2022-09-13 01:56:14 -0300

    insert implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -10,8 +11,10 @@ public class Program2 {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-		
+
 		System.out.println("=== TEST 1: department findByID =====");
 		Department department = departmentDao.findById(2);
 		System.out.println(department);
@@ -21,5 +24,12 @@ public class Program2 {
 		for (Department obj : departamento) {
 			System.out.println(obj);
 		}
+
+		System.out.println("\n===== TEST 3: seller insert ====");
+		System.out.println("Digite um nome para inserir no banco: ");
+		String nome = sc.next();
+		Department newDepart = new Department(1, nome);
+		departmentDao.insert(newDepart);
+		System.out.println("Inserted! New Id = " + newDepart.getId());
 	}
 }
